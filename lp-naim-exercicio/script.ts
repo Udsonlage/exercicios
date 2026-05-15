57d3db91eb69d3d294b622e32ce945354e9f353e
Udsonlage <udson.ramos@naimseguro.com.br>
2026-05-15 17:41:47 -0300

    atualização pratica lp naim

--- lp-naim-exercicio/script.ts
+++ lp-naim-exercicio/script.ts
@@ -21,3 +21,49 @@ class MenuHamburguer{
 }
 
 const meuMenu = new MenuHamburguer("#botaoHamb", "lista")
+
+// Ícones e imagens
+
+class IconesEImagens{
+    private icones: NodeListOf<HTMLButtonElement>;
+    private imagens: NodeListOf<HTMLDivElement>;
+    private classeAtiva: string
+
+    //public estaAtivo: boolean = false;
+    constructor(classeAtivaCSS: string = ".imgsAtivo"){
+        this.icones = document.querySelectorAll<HTMLButtonElement>(".icons")
+        this.imagens = document.querySelectorAll<HTMLDivElement>(".imgs")
+        this.classeAtiva = classeAtivaCSS
+
+        this.inicializar()
+    }
+
+    private inicializar(): void {
+        
+        this.icones.forEach((icone) => {
+
+            icone.addEventListener("click",() => {
+                const idDaImagemAlvo = icone.dataset.target
+            
+                if(idDaImagemAlvo){
+                    this.alternarImagens(idDaImagemAlvo)
+                }
+            })
+        })
+    }
+
+    private alternarImagens(idAlvo: string){
+
+        this.imagens.forEach((img) => {
+            img.classList.remove(this.classeAtiva)
+        })
+
+        const imagemExibir = document.getElementById(idAlvo) as HTMLDivElement
+
+        if(imagemExibir){
+            imagemExibir.classList.add(this.classeAtiva)
+        }
+    }
+}
+
+const galera = new IconesEImagens("imgsAtivo")
